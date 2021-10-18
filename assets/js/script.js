4b5fa141cb90cf015e1357babb2b592449cb2a62
adairconlin <adairconlin@gmail.com>
2021-10-18 16:58:59 -0400

    added length prompt function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,41 @@
-// Assignment code here
+// Prompts user for input on their desired length of the generated password.
+let length = function() {
+    let passwordLength = window.prompt("Please select a length for your password. Length must be greater than 8 and fewer than 128.");
+    // Changes their response from a string to an number.
+    passwordLength = parseInt(passwordLength);
+    console.log(passwordLength);
 
+    // Checks for any null or invalid inputs. Restarts prompt if it is invalid.
+    if(!passwordLength) {
+        window.alert("Invalid entry, please select a valid length.");
+        return length();
+    }
+
+    // Checks if their input meets the length requirements. Restarts prompt if it is invalid.
+    if(passwordLength <= 8 || passwordLength >= 128) {
+        window.alert("Invalid entry, please select a valid length.");
+        return length();
+    } 
+}
+
+// Calls length() and characters() functions to begin prompts for the password.
+let criteriaPrompts = function() {
+    length();
+}
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//
+criteriaPrompts();
